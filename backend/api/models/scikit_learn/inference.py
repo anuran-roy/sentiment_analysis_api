241a9c703ff60bb864b46db2f404df68047ba1d9
Anuran Roy <anuranroy02@gmail.com>
2022-12-26 00:01:22 +0530

    Modified inference method to suit model execution and output.

--- backend/api/models/scikit_learn/inference.py
+++ backend/api/models/scikit_learn/inference.py
@@ -1,1 +1,31 @@
-from sklearn.ensemble import RandomForestClassifier
+import pickle
+from typing_extensions import Literal
+from settings import BASE_DIR
+from .model import TextClassifier
+
+
+def infer(sentence: str) -> Literal["positive", "negative"]:
+    # model = TextClassifier(vectorizer_fit=True)
+    # model.load_model(
+    #     classifier="/media/anuran/Samsung SSD 970 EVO 1TB/Internship/TrueFoundry/Internship Task/backend/api/models/scikit_learn/weights/classifier/best.pkl",
+    #     vectorizer="/media/anuran/Samsung SSD 970 EVO 1TB/Internship/TrueFoundry/Internship Task/backend/api/models/scikit_learn/weights/vectorizer/best.pkl",
+    # )
+    # return model.inference(model.vectorize(sentence))
+
+    vectorizer = pickle.load(
+        open(
+            "/media/anuran/Samsung SSD 970 EVO 1TB/Internship/TrueFoundry/Internship Task/backend/api/models/scikit_learn/weights/vectorizer/best.pkl",
+            "rb",
+        )
+    )
+    classifier = pickle.load(
+        open(
+            "/media/anuran/Samsung SSD 970 EVO 1TB/Internship/TrueFoundry/Internship Task/backend/api/models/scikit_learn/weights/classifier/best.pkl",
+            "rb",
+        )
+    )
+
+    sentence = vectorizer.transform([sentence])
+    response = classifier.predict(sentence)
+    print(response)
+    return response[0]
